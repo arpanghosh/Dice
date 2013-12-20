7e44a8436adb5a630f3dfcaca239c48b027135b3
Arpan Ghosh <arpan@tracktorbeam.com>
2013-12-19 22:53:23 -0800

    Added a ‘Launch’ ViewController, to be displayed as a welcome screen, so that full screen ads can be displayed during the segue from this controller to the main controller.

---
+++ SampleIOSAppUsingPlanB/SampleAppLaunchViewController.h
@@ -0,0 +1,15 @@
+//
+//  SampleAppLaunchViewController.h
+//  SampleIOSAppUsingPlanB
+//
+//  Created by Arpan Ghosh on 12/19/13.
+//  Copyright (c) 2013 Plan B. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SampleAppViewController.h"
+
+@interface SampleAppLaunchViewController : UIViewController
+
+@end
---
+++ SampleIOSAppUsingPlanB/SampleAppLaunchViewController.m
@@ -0,0 +1,55 @@
+//
+//  SampleAppLaunchViewController.m
+//  SampleIOSAppUsingPlanB
+//
+//  Created by Arpan Ghosh on 12/19/13.
+//  Copyright (c) 2013 Plan B. All rights reserved.
+//
+
+#import "SampleAppLaunchViewController.h"
+
+
+@interface SampleAppLaunchViewController ()
+
+@end
+
+
+@implementation SampleAppLaunchViewController
+
+
+-(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
+    /*
+    UIViewController *destinationViewController = [segue destinationViewController];
+    destinationViewController.interstitialPresentationPolicy = ADInterstitialPresentationPolicyAutomatic;
+     */
+}
+
+
+- (BOOL)canBecomeFirstResponder {
+    return YES;
+}
+
+
+-(void)motionEnded:(UIEventSubtype)motion withEvent:(UIEvent *)event{
+    if (motion == UIEventSubtypeMotionShake) {
+        [self performSegueWithIdentifier:@"LAUNCH_TO_MAIN" sender:self];
+    }
+}
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	
+    [self becomeFirstResponder];
+}
+
+
+-(void)logWithMessage:(NSString *)message andError:(NSError *)error{
+    NSLog(@"%@ : %@\n%@ : %@", NSStringFromClass([self class]), message,
+          [error localizedDescription],
+          [error localizedFailureReason]);
+}
+
+
+@end
